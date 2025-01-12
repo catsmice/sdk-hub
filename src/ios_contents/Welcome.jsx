e04d4dc52282f0358671caefd6fb63d33105fc62
Jim Lee <catsmice@gmail.com>
2025-01-12 18:14:42 +0800

    APIDocIOS update

---
+++ src/ios_contents/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Welcome = () => (
+    <>
+      <h2>Welcome</h2>
+      <p className="text-gray-500">Welcome2 to the iOS SDK documentation! Here you'll find all the resources to get started.</p>
+    </>
+  );
+  export default Welcome;
